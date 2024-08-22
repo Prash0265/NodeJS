5f9631a85ed40925b0051f91076c6a3674743d23
Nandaram Prashanth kumar <kmrprashanth92@gmail.com>
2024-08-22 15:50:58 -0400

    EP 4 end

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+//require("../Node js/xyz");
 
-const {calculateSum}=require("./sum");
-const {calculateMultiply} = require("./multiply")
-const {data}=require("./data.json");
-console.log('test :>> ', data);
+// const { calculateMutiply } = require("./calculate/multiply");
+// const {calculateSum}= require("./calculate/sum");
+//import {x, calculateSum } from "./sum.js";
+const {calculateMultiply,calculateSum} = require("./calculate");
+const data = require("./data.json");
 
-var a = 10;
-var b = 20;
+console.log('data :>> ', data);
 
+var a =10;
+var b =20;
 calculateSum(a,b);
 calculateMultiply(a,b);
+
+console.log('a+b :>> ', a+b);
+
+///console.log(global);
 
